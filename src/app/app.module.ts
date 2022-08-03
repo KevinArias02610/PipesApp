e26638a9c3cfef33a62242eee5c57d9628af47a7
julianarias <kjulianr41@gmail.com>
2022-08-03 17:42:09 -0500

    Seccion 10 Clase 146(DatePipe)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRouterModule } from './app-router.module';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { VentasModule } from './ventas/ventas.module';
+
+import localeEsCol from '@angular/common/locales/es-CO';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeEsCol);
 
 @NgModule({
   declarations: [
@@ -17,7 +22,9 @@ import { VentasModule } from './ventas/ventas.module';
     SharedModule,
     VentasModule,
   ],
-  providers: [],
+  providers: [{
+    provide: LOCALE_ID, useValue: 'es-CO'
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/ventas/pages/basicos/basicos.component.ts
+++ src/app/ventas/pages/basicos/basicos.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html'
 })
-export class BasicosComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class BasicosComponent {
+  public nombreLower: string = 'julián';
+  public nombreUpper: string = 'JULIÁN';
+  public nombreCompleto: string = 'jUlIaN aRiAs';
+  public original: string = 'Original: ';
+  
+  public fecha: Date = new Date();
 }
