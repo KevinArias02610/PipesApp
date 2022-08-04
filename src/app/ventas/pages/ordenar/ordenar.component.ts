54ffa4c6f9d5d9f63782885c2ce4e04e4e582df0
julianarias <kjulianr41@gmail.com>
2022-08-04 18:48:07 -0500

    Seccion 11 Clase 169 (Fin seccion 11, Sortable table)

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,14 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interfaces';
 
 @Component({
   selector: 'app-ordenar',
   templateUrl: './ordenar.component.html'
 })
-export class OrdenarComponent implements OnInit {
+export class OrdenarComponent {
 
-  constructor() { }
+  public enMayusculas: boolean = true;
+  public ordenarPor: string = '';
 
-  ngOnInit(): void {
+  public heroes: Heroe[] = [
+    {nombre: 'Superman', vuela: true, color: Color.azul},
+    {nombre: 'Robin', vuela: false, color: Color.verde},
+    {nombre: 'Spiderman', vuela: false, color: Color.rojo},
+    {nombre: 'Ironman', vuela: true, color: Color.rojo},
+    {nombre: 'Batman', vuela: false, color: Color.negro},
+  ];
+
+  public heroes2: Heroe[] = [...this.heroes];
+
+  modificarParametro(){
+    this.enMayusculas = !this.enMayusculas;
+  };
+
+  cambiarOrden(valor: string){
+    this.ordenarPor = valor;
   }
 
 }
